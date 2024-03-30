c616ffd8003b36eab7d2cd20a6b00f97cbec4cf2
syed faris <132473241+syedfariskarachi@users.noreply.github.com>
2024-03-30 13:28:00 +0500

    TODOS_LISR

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+  let addtasks = await inquirer.prompt([
+    {
+      message: "What you want to add in your Todos!",
+      name: "todo",
+      type: "input",
+    },
+
+    {
+      message: "Do you want to add more!",
+      name: "addmore",
+      type: "confirm",
+      default: "false",
+    },
+  ]);
+  todos.push(addtasks.todo);
+  condition = addtasks.addmore;
+  console.log(todos);
+}
